c07aecbf20ab2f704183c5d8f9b879b4a2fb48b7
Om Nikhade <omnikhade@hotmail.com>
2024-09-12 00:34:00 +0530

    Create main.tf

---
+++ Terraform-files/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+  key_name = "mykey"
+  vpc_security_group_ids = ["sg-02c7dbd4907391ff7"]
+  connection {
+     type = "ssh"
+     user = "ubuntu"
+     private_key = file("./mykey.pem")
+     host = self.public_ip
+     }
+  provisioner "remote-exec" {
+     inline = ["echo 'wait to start the instance' "]
+  }
+  tags = {
+     Name = "test-server"
+     }
+  provisioner "local-exec" {
+     command = "echo ${aws_instance.test-server.public_ip} > inventory"
+     }
+  provisioner "local-exec" {
+     command = "ansible-playbook /var/lib/jenkins/workspace/banking-finance-prj/terraform-files/ansibleplaybook.yml"
+     }
+  }
