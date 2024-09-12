0dc5c6dfc2c1548db6f4ec452a6c6f8ec08ef912
Om Nikhade <omnikhade@hotmail.com>
2024-09-12 10:27:34 +0530

    Updated main.tf

--- Terraform-files/main.tf
+++ Terraform-files/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-server" {
+resource "aws_instance" "deploy-server" {
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
   key_name = "mykey"
@@ -13,10 +13,10 @@ resource "aws_instance" "test-server" {
      inline = ["echo 'wait to start the instance' "]
   }
   tags = {
-     Name = "test-server"
+     Name = "deploy-server"
      }
   provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
+     command = "echo ${aws_instance.deploy-server.public_ip} > inventory"
      }
   provisioner "local-exec" {
      command = "ansible-playbook /var/lib/jenkins/workspace/banking-finance-prj/terraform-files/ansibleplaybook.yml"
