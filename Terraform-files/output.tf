a99dccae2cfc30f19bac2c3c154fafc573e87e13
Om Nikhade <omnikhade@hotmail.com>
2024-09-12 10:24:55 +0530

    Updated output.tf

--- Terraform-files/output.tf
+++ Terraform-files/output.tf
@@ -1,4 +1,4 @@
 output "ec2_public_ip" {
   description = "Public IP of the EC2 instance"
-  value       = aws_instance.test-server.public_ip
+  value       = aws_instance.deploy-server.public_ip
 }
